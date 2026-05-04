37c1731f17cbf986c11cc0081e7f1aaa72c0ce1c
parkjeehoon <gkstj8300@naver.com>
2026-05-04 13:38:56 +0900

    refactor(home): FirstSection의 이력 버튼 제거

--- apps/home/src/features/home/ui/FirstSection/index.tsx
+++ apps/home/src/features/home/ui/FirstSection/index.tsx
@@ -1,13 +1,6 @@
-import { url } from '@jeehoon/utils';
-import Link from 'next/link';
 import styles from './FirstSection.module.scss';
-import { ga } from '@/shared/lib/logs/analytics';
 
 export default function FirstSection() {
-  const handleResumeClick = () => {
-    ga.events.resumeView();
-  }
-
   return (
     <section className={styles.firstSection}>
       <div className={styles.firstWrap}>
@@ -19,13 +12,6 @@ export default function FirstSection() {
             <rp>)</rp>
           </ruby>
         </h1>
-        <Link 
-          className={styles.resumeBtn} 
-          href={url.resume}
-          onClick={() => handleResumeClick()}
-        >
-          이력
-        </Link>
       </div>
       <h2 className={styles.developer}>FRONT-END 개발자</h2>
     </section>
